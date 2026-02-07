9787ff254854f7101a381a3ebf0526b675376b83
Ryohskay <learnerryo@gmail.com>
2026-02-07 23:47:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Diary edited with Gemini
+## getting started
+* set `GEMINI_API_KEY` envvar 
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"google.golang.org/genai"
+)
+
+func make_prompt(target_lang string, reply_lang string, diary string) string {
+	msg := " If there are unatural wording, suggest improvements to make the sentences sound more natural. "
+	return fmt.Sprintf("Here is my diary entry in %s. Correct grammar errors and spelling mistakes.%sExplanation should be in %s.\n# start of the diary entry\n%s\n# end of the diary entry",
+										target_lang, msg, reply_lang, diary)
+}
+
+func call_gemini(prompt string) string {
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := client.Models.GenerateContent(
+		ctx,
+		"gemini-3-flash-preview",
+		genai.Text(prompt),
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result.Text()
+}
+
+func main() {
+	lang := "English"
+	interface_lang := "Japanese"
+	diary := "I'm tired today because I did a lot of things to write my documents to apply to become a guide-interpreter in Japan."
+	reply := call_gemini(make_prompt(lang, interface_lang, diary))
+  fmt.Println(reply)
+}
